cc7efb3c570316c7068f07748eb57fbaf2ae4df0
Md Towhidul Alam <158472221+mdtowhid98@users.noreply.github.com>
2024-08-21 13:11:45 +0600

    21.08.2024

---
+++ springBootProject4/springBootProject4/src/main/java/com/towhid/springBootProject4/SpringBootProject4Application.java
@@ -0,0 +1,13 @@
+package com.towhid.springBootProject4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject4Application.class, args);
+	}
+
+}
---
+++ springBootProject4/springBootProject4/src/test/java/com/towhid/springBootProject4/SpringBootProject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.towhid.springBootProject4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
