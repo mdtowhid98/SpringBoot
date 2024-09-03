02d4239f9de66f789877b6079c5bf2930717d3d2
Md Towhidul Alam <158472221+mdtowhid98@users.noreply.github.com>
2024-09-04 00:44:33 +0600

    add

---
+++ hotelmanagement/src/main/java/com/towhid/hotelmanagement/entity/Hotel.java
@@ -0,0 +1,31 @@
+package com.towhid.hotelmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Hotels")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+    private  String name;
+    private  String address;
+    private  String rating;
+    private  float minPrice;
+    private  float maxPrice;
+    private  String image;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "laoctionId")
+    private  Location location;
+
+
+}
---
+++ hotelmanagement/src/main/java/com/towhid/hotelmanagement/repository/HotelRepository.java
@@ -0,0 +1,17 @@
+package com.towhid.hotelmanagement.repository;
+
+import com.towhid.hotelmanagement.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel,Integer> {
+
+    @Query("SELECT h FROM Hotel h WHERE h.location.name = :locationName")
+    List<Hotel> findHotelsByLocationName(@Param("locationName") String locationName);
+
+}
