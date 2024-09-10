60081dac8ca348a0fd3aca248f663a9b33c2342b
Md Towhidul Alam <158472221+mdtowhid98@users.noreply.github.com>
2024-09-10 11:08:40 +0600

    add

---
+++ pharmacyManagement/src/main/java/com/towhid/pharmacyManagement/sequrity/SequrityConfig.java
@@ -0,0 +1,70 @@
+package com.towhid.pharmacyManagement.sequrity;
+
+import com.towhid.pharmacyManagement.jwt.JwtAuthenticationFilter;
+import com.towhid.pharmacyManagement.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+
+public class SequrityConfig {
+
+    private final UserService userService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return
+                http
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(
+
+                                req ->
+                                        req.requestMatchers("/login", "/register", "api/medicine/", "api/medicinegeneric/","api/pharmacist/")
+                                                .permitAll()
+                                                .requestMatchers("api/medicine/save", "api/medicinegeneric/save")
+                                                .hasAuthority("ADMIN")
+                                                .requestMatchers("api/medicinegeneric/{id}", "api/medicine/{id}","api/pharmacist/all/**", "api/location/")
+                                                .hasAnyAuthority("ADMIN", "PHARMACIST")
+                                                .requestMatchers("api/user/**")
+                                                .hasAuthority("USER")
+
+                        )
+                        .userDetailsService(userService)
+                        .sessionManagement(
+                                session ->
+                                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        )
+                        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                        .build();
+
+
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+}
